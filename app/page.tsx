9b2fc796891a6e23528cdc52366164dc76486e3a
airflowboy <ajtwoddl9194@gmail.com>
2025-01-18 00:56:17 +0900

    tailwind, shadcn, supabase 설정 완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
   return (
-    <div>Hello</div>
+    <div className="text-3xl font-bold flex-col items-center">
+      <h1>Welcome Home page</h1>
+      <Button className="w-full">Welcome</Button>
+    </div>
   )
 }
